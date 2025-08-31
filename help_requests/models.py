6bee39bacdf2b2d3bf64b725e8afebce0969e1f2
kundan <kundanbhardwaj145@gmail.com>
2025-08-31 14:45:01 +0530

    commit

--- help_requests/models.py
+++ help_requests/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 # Create your models here.
 class HelpRequest(models.Model):
     description = models.TextField()
-    aadhar_id = models.IntegerField()
+    aadhar_id = models.CharField(max_length=100)
     date = models.DateTimeField(auto_now_add=True)
     location = models.CharField(max_length=10000)
     status = models.CharField(max_length=100, default='open')
     image = models.ImageField(upload_to='help_request_images/', null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=100)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=100)
     bank_details = models.ForeignKey('BankDetails', on_delete=models.CASCADE, null=True, blank=True)
 
 class BankDetails(models.Model):
--- volunteers/models.py
+++ volunteers/models.py
@@ -7,5 +7,5 @@ from django.contrib.auth.models import User
 class Volunteer(models.Model):
     aadhar_id = models.IntegerField(unique=True)
     location = models.CharField(max_length=10000)
-    phone_number = models.IntegerField()
+    phone_number = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='volunteer_profile')
